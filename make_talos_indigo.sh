f818accc12f3b3d480efef5837b84c0ecdd87389
dash <dash@dash-ThinkPad-T530.(none)>
2015-05-15 11:05:33 -0400

    Added make indigo script

---
+++ make_talos_indigo.sh
@@ -0,0 +1,27 @@
+# Author: Devon Ash
+# BSD, UBC Open Robotics.
+
+BLACK='\033[0;31m'
+RED='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+echo -e "${BLACK}Initializing developer workspace...${NC}\n"
+# Setup file structure
+mkdir -p ~/talos_hydro_workspace/src
+catkin_init_workspace ~/talos_hydro_workspace/src
+echo -e "\n${RED}Initializing developer workspace [Complete]${NC}\n"
+
+echo -e "\n${BLACK}Downloading packages...${NC}\n"
+# Download packages
+rosinstall indigo .
+mv hydro/talos_* ~/talos_hydro_workspace/src
+echo -e "\n${RED}Downloading packages [Complete]${NC}\n"
+
+echo -e "\n${BLACK}Building packages...${NC}\n"
+# Build packages
+cd ~/talos_hydro_workspace/
+catkin_make
+echo -e "\n${RED}Building packages [Complete]${NC}\n"
+
+echo -e "\n${YELLOW}You're ready to start developing!${NC}\n"
